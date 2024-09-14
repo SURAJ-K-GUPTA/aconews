4dcbd280cad5adac0a6c170869e8d105eb5a4951
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2024-09-14 11:23:59 +0530

    firebase

--- aconews-frontend/src/config.js
+++ aconews-frontend/src/config.js
@@ -1,9 +1,15 @@
 // config.js
+// const config = {
+//     apiBaseUrl: process.env.NODE_ENV === 'development'
+//       ? 'http://localhost:5000'
+//       : 'https://aconews-backend-nine.vercel.app'
+//   };
+  
+//   export default config;
+  
+
 const config = {
-    apiBaseUrl: process.env.NODE_ENV === 'development'
-      ? 'http://localhost:5000'
-      : 'https://aconews-backend-nine.vercel.app'
-  };
-  
-  export default config;
-  
+  apiBaseUrl: process.env.REACT_APP_API_BASE_URL
+};
+
+export default config;
