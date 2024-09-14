a2223f5a21e38d5c26d6e1e70945f8302a0942b5
SURAJ-K-GUPTA <116446956+SURAJ-K-GUPTA@users.noreply.github.com>
2024-09-14 09:39:03 +0530

    config

--- aconews-frontend/src/config.js
+++ aconews-frontend/src/config.js
@@ -2,7 +2,7 @@
 const config = {
     apiBaseUrl: process.env.NODE_ENV === 'development'
       ? 'http://localhost:5000'
-      : 'https://aconews-backend-cdh8goyw4-suraj-kumar-guptas-projects.vercel.app/'
+      : 'https://aconews-backend-nine.vercel.app/'
   };
   
   export default config;
